d05b1f3e943fecf071981b37a12af4b7a8e48507
AnastasiiaRudch <anastasiag1409@gmail.com>
2025-06-23 23:10:11 +0300

    DART-06 Завдання 2: Оператори порівняння

--- lib/1_task.dart
+++ lib/1_task.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 void main() {
   task1_rectangle();
+  task2_comparison();
 }
 
-void task1_rectangle(){
-
+void task1_rectangle() {
   double width = 18.4;
   double length = 30.9;
 
@@ -13,5 +15,21 @@ void task1_rectangle(){
 
   print('Площа: $S');
   print('Периметр: $P');
-  print('fullSquares: $fullSquares');
+  print('fullSquares: $fullSquares\n');
+}
 
+void task2_comparison(){
+  final random = new Random();
+  int number = random.nextInt(200);
+
+  if(number > 50){
+    print('$number is bigger than 50');
+  }
+  if(number < 100){
+    print('$number  is less than 100');
+  }
+  if(number % 5 == 0){
+    print('$number is divisible by 5\n');
+  }
+
+}
